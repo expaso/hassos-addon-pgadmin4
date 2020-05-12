87ad3d79de8f371b96c05152944d3f59006e9bf6
Hans <hes@optimizers.nl>
2020-05-12 21:52:11 +0200

    Fixed typo

--- pgadmin4/rootfs/etc/cont-init.d/nginx.sh
+++ pgadmin4/rootfs/etc/cont-init.d/nginx.sh
@@ -35,7 +35,7 @@ ingress_interface=$(bashio::addon.ip_address)
 ingress_entry=$(bashio::addon.ingress_entry)
 sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/servers/ingress.conf
 sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf
-sed -i "s/%%entry%%/${ingress_entry}/g" /etc/nginx/servers/ingress.conf
+sed -i "s#%%entry%%#${ingress_entry}#g" /etc/nginx/servers/ingress.conf
 
 dns_host=$(bashio::dns.host)
 sed -i "s/%%dns_host%%/${dns_host}/g" /etc/nginx/includes/resolver.conf
