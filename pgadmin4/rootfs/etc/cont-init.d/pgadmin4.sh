95395dbb6d358f1112732375f4c92ee7bbd4a3df
Hans van Essen <hes@optimizers.nl>
2020-10-13 09:06:37 +0200

    - Upgraded paAdmin to 4.26 - Refactored patching pgAdmin from using SED to git apply patch.

--- pgadmin4/rootfs/etc/cont-init.d/pgadmin4.sh
+++ pgadmin4/rootfs/etc/cont-init.d/pgadmin4.sh
@@ -30,10 +30,16 @@ if [ ! -f /data/pgadmin4/pgadmin4.db ]; then
     python run_pgadmin.py || bashio::exit.nok "Failed to initialize Database"
 fi
 
+
+
+bashio::log.info "Patching pgAdmin to work with HA ingress.."
 # Patch pgadmin-files to allow reverse proxying in desktop mode.
 # This is needed to support ingress (which is on a deeper path below root)
 # We want to run pfAdmin in desktop-mode (no users), because we would like to handle users and authentication ourselves.
 # Problem is, in desktop mode, reverse-propxying is not respected.
 # And so we patch the files to enale this behaviour again.
-sed -i '105s/config.SERVER_MODE/True/' /pgadmin4/pgadmin/__init__.py    #Line 105: Assume server-mode here to enable reverse-proxy
-sed -i '95s/config.SERVER_MODE/True/' /pgadmin4/pgAdmin4.py             #Line 94: Assume server-mode here to enable reverse-proxy
+#sed -i '105s/config.SERVER_MODE/True/' /pgadmin4/pgadmin/__init__.py    #Line 105: Assume server-mode here to enable reverse-proxy
+#sed -i '95s/config.SERVER_MODE/True/' /pgadmin4/pgAdmin4.py             #Line 94: Assume server-mode here to enable reverse-proxy
+cd /pgadmin4
+git apply -v /etc/cont-init.d/pgAdmin.patch
+
