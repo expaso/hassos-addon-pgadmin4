ec9cd7fb798a86d84deef19c43a322ded33ad7d4
Hans van Essen <hes@optimizers.nl>
2021-03-03 09:03:11 +0100

    Upgrade to pgAdmin 5.0

--- pgadmin4/rootfs/etc/cont-init.d/pgadmin4.sh
+++ pgadmin4/rootfs/etc/cont-init.d/pgadmin4.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/with-contenv bashio
 # ==============================================================================
 # Home Assistant Community Add-on: pgAdmin4
-# Configures pgAdmin4 before running
+# Configures pgAdmin4 before running (see official-pgadmin4/pkg/docker/entrypoint.sh )
 # ==============================================================================
+
 
 # Populate config_distro.py. This has some default config, as well as anything
 # provided by the user through the PGADMIN_CONFIG_* environment variables.
@@ -10,11 +11,19 @@
 # container build so it can have the required ownership.
 if [ `wc -m /pgadmin4/config_distro.py | awk '{ print $1 }'` = "0" ]; then
     cat << EOF > /pgadmin4/config_distro.py
+CA_FILE = '/etc/ssl/certs/ca-certificates.crt'
+LOG_FILE = '/dev/null'
 HELP_PATH = '../../docs'
 DEFAULT_BINARY_PATHS = {
-        'pg': '/usr/local/pgsql-12'
+        'pg': '/usr/local/pgsql-13'
 }
 EOF
+
+    # This is a bit kludgy, but necessary as the container uses BusyBox/ash as
+    # it's shell and not bash which would allow a much cleaner implementation
+    for var in $(env | grep PGADMIN_CONFIG_ | cut -d "=" -f 1); do
+        echo ${var#PGADMIN_CONFIG_} = $(eval "echo \$$var") >> /pgadmin4/config_distro.py
+    done
 fi
 
 # Ensure configuration exists
@@ -27,9 +36,8 @@ if [ ! -f /data/pgadmin4/pgadmin4.db ]; then
     # Initialize DB before starting Gunicorn
     # Importing pgadmin4 (from this script) is enough
     cd /pgadmin4
-    python run_pgadmin.py || bashio::exit.nok "Failed to initialize Database"
+    /venv/bin/python3 run_pgadmin.py || bashio::exit.nok "Failed to initialize Database"
 fi
-
 
 
 bashio::log.info "Patching pgAdmin to work with HA ingress.."
