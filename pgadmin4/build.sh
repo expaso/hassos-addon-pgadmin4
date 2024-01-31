de2c1e5883b429294822fd1d04474b77548f2e8f
Hans van Essen <Expaso@users.noreply.github.com>
2024-01-31 11:26:37 +0100

    Worked on multiarch build

--- pgadmin4/build.sh
+++ pgadmin4/build.sh
@@ -12,12 +12,36 @@ fi
 # Print the result
 echo "Building version '$version' for platforms '$archs'"
 
+# error: failed to solve: ghcr.io/hassio-addons/base/armv7:15.0.5: error getting credentials - err: exec: "docker-credential-desktop.exe": executable file not found in $PATH, out: `
+#Solution:
+#In ~/.docker/config.json change credsStore to credStore
+
+
+#docker run --rm --privileged \
+#    -v ~/.docker:/root/.docker \
+#    -v /var/run/docker.sock:/var/run/docker.sock \
+#    -v ~/hassos-addon-pgadmin4/pgadmin4:/data homeassistant/amd64-builder \
+#    --addon \
+#    --target pgadmin4 \
+#    --all \
+#    --cache-tag cache \
+#    -t /data
+
 # Build and Push
-#    
-docker buildx build \
-    --push \
-    --platform aarch64 \
-    --cache-from type=registry,ref=husselhans/hassos-addon-pgadmin4:cache \
-    --cache-to type=registry,ref=husselhans/hassos-addon-pgadmin4:cache \
-    --tag husselhans/hassos-addon-pgadmin4:$version \
-    .
+
+# For each arch in config.yaml, run the buildx build command
+
+# Get the archs from the config.yaml file using yq
+for arch in $(yq -r '.arch[]' config.yaml); do
+    echo "Building for: $arch"
+    # build the image
+    docker buildx build \
+        --push \
+        --platform linux/$arch \
+        --cache-from type=registry,ref=husselhans/hassos-addon-pgadmin4:cache \
+        --cache-to type=registry,ref=husselhans/hassos-addon-pgadmin4:cache,mode=max \
+        --tag husselhans/hassos-addon-pgadmin4-$arch:$version \
+        --build-arg BUILD_FROM=ghcr.io/hassio-addons/base/$arch:15.0.5 \
+        --progress plain \
+        .
+done
